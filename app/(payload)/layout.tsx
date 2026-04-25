3f9da791315f2e47f5f6b71a67480e4cbce2ed6a
Esteban <estebaneqstek@gmail.com>
2026-04-25 14:23:21 -0300

    fix bug vercel

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -1,18 +1,13 @@
-import { RootLayout, handleServerFunctions } from '@payloadcms/next/layouts'
+import { RootLayout } from '@payloadcms/next/layouts'
 import config from '@/payload.config'
 import { importMap } from './admin/importMap'
-import type { ServerFunctionClientArgs } from 'payload'
+import { serverFunction } from './actions'
 import React from 'react'
 import '@payloadcms/next/css'
 
 export { metadata } from '@payloadcms/next/layouts'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  async function serverFunction(args: ServerFunctionClientArgs) {
-    'use server'
-    return handleServerFunctions({ ...args, config, importMap })
-  }
-
   return (
     <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
       {children}
